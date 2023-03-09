d5d9e87edd5ec747719b151c2a96dc11c2deb543
Stefanie Schutera <stefanie.schutera@mailbox.org>
2023-03-09 16:45:47 -0700

    Fix json issue

--- ModelRunTest.py
+++ ModelRunTest.py
@@ -4,6 +4,7 @@ import csv
 import matplotlib.pyplot as plt
 import pandas as pd
 import csv
+import json
 
 
 from core.CooperationModel import CooperationModel
@@ -16,32 +17,17 @@ for agent in testModel.agents:
 for index in range(3):
     statsPerGen = testModel.step()
     statsPerGen.printStats()
+
 testModel.plot_network()
 
 for agent in testModel.agents:
     print(agent.ID, agent.fitness, agent.tag, agent.tolerance)
 
-myfile = open("C:\\MAIN-FOLDER-TO-BACKUP\\programming\\My-Python\\ABM_EvolutionOfCooperation\\TestData.csv", 'r')
-csv_reader = csv.reader(myfile, delimiter = ',')
-xvalues = []
-yvalues = []
 
-for line in csv_reader:
-    print(line)
-    xvalues.append(line[0])
-    yvalues.append(line[1])
+# Opening JSON file
+with open('output/20230309_163856_output.json', 'r') as openfile:
 
-ax = plt.axes()
-plt.plot(xvalues, yvalues, color = 'g',
-         linestyle = 'dashed', marker = 'o',
-         label = "R = 1")
-plt.xlabel('Pairings')
-plt.ylabel('Mean Donation Rate')
+    # Reading from json file
+    output = json.load(openfile)
 
-ax.set_xticks(xvalues)
-ax.set_yticks(yvalues)
-plt.title('Sensitivity Analysis', fontsize = 20)
-plt.legend()
-plt.show()
-
-myfile.close()
+print(output[0]["parameters"]["numberOfSteps"])
