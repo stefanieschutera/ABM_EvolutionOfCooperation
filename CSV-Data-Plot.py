0124da9b29db84737856ef6d405f2e7e86faf511
Rohan <rykorde@asu.edu>
2023-03-09 17:49:12 -0700

    Add code to plot graph from JSON data file

--- CSV-Data-Plot.py
+++ CSV-Data-Plot.py
@@ -2,6 +2,7 @@
 import csv
 
 import matplotlib.pyplot as plt
+import pandas as pd
 import csv
 import json
 
@@ -16,31 +17,25 @@ for agent in testModel.agents:
 for index in range(3):
     statsPerGen = testModel.step()
     statsPerGen.printStats()
-
 testModel.plot_network()
 
 for agent in testModel.agents:
     print(agent.ID, agent.fitness, agent.tag, agent.tolerance)
 
-
-# Opening JSON file
-with open('output/20230309_163856_output.json', 'r') as openfile:
-    # Reading from json file
-    output = json.load(openfile)
-
-
+myfile = open("C:\\MAIN-FOLDER-TO-BACKUP\\programming\\My-Python\\ABM_EvolutionOfCooperation\\TestData.csv", 'r')
+csv_reader = csv.reader(myfile, delimiter = ',')
 xvalues = []
 yvalues = []
-radiusForMateSelection = output[0]["parameters"]["radiusForMateSelection"]
 
-for index in range(len(output)):
-    xvalues.append(output[index]["parameters"]["numberOfPairings"])
-    yvalues.append(output[index]["meanDonationRateOfLastSteps"])
+for line in csv_reader:
+    print(line)
+    xvalues.append(line[0])
+    yvalues.append(line[1])
 
 ax = plt.axes()
 plt.plot(xvalues, yvalues, color = 'g',
          linestyle = 'dashed', marker = 'o',
-         label = "R = "+  str(radiusForMateSelection) + " ")
+         label = "R = 1")
 plt.xlabel('Pairings')
 plt.ylabel('Mean Donation Rate')
 
@@ -50,4 +45,17 @@ plt.title('Sensitivity Analysis', fontsize = 20)
 plt.legend()
 plt.show()
 
-openfile.close()
+myfile.close()
+
+# JSON file processing
+
+jsonfile = open("C:\\MAIN-FOLDER-TO-BACKUP\\programming\\My-Python\\ABM_EvolutionOfCooperation\\TestJSON.json")
+jsondata = json.load(jsonfile)
+
+for i in jsondata:
+    print(i)
+
+jsonfile.close()
+
+
+
